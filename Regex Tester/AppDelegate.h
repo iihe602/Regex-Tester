0afd8e6c0704d0f94d69f773484c5ab4e85e720c
iihe602 <iihe602@qq.com>
2013-01-16 09:37:18 +0800

    Initial Commit

---
+++ Regex Tester/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Regex Tester
+//
+//  Created by wangyongqi on 1/16/13.
+//  Copyright (c) 2013 wyq. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Regex Tester/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Regex Tester
+//
+//  Created by wangyongqi on 1/16/13.
+//  Copyright (c) 2013 wyq. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
